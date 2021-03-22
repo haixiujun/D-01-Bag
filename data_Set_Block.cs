a1b419bb075d734a401e8a427aa7313922f56b02
AoYangJia <1357878465@qq.com>
2021-03-22 13:52:38 +0800

    增加了数据集和项集的初始化函数

--- data_Set_Block.cs
+++ data_Set_Block.cs
@@ -8,6 +8,23 @@ namespace D_01_Bag
     {
         private int item_Count;
         private int bag_Cubage;
+        private item_Set[] item_Sets;
+
+        public data_Set_Block(int iC,int bC)
+        {
+            item_Count = iC;
+            bag_Cubage = bC;
+            item_Sets = new item_Set[item_Count];
+        }
+
+        public void init_Item_Sets(int[] profit,int[] weight)
+        {
+            for(int i = 0; i < item_Count; i++)
+            {
+                item_Sets[i].init_Profit(profit[i * 3], profit[i * 3 + 1], profit[i * 3 + 2]);
+                item_Sets[i].init_Weight(weight[i * 3], weight[i * 3+1], weight[i * 3+2]);
+            }
+        }
 
 
 
