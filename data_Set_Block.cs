95140a4ad291fddc701c97147a0c64b9db6c44c5
AoYangJia <1357878465@qq.com>
2021-03-22 13:46:59 +0800

    增加了项集类

--- data_Set_Block.cs
+++ data_Set_Block.cs
@@ -6,5 +6,11 @@ namespace D_01_Bag
 {
     class data_Set_Block
     {
+        private int item_Count;
+        private int bag_Cubage;
+
+
+
+
     }
 }
---
+++ item_Set.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D_01_Bag
+{
+    class item_Set
+    {
+        private int[] profit;
+        private int[] weight;
+        public item_Set()
+        {
+            profit = new int[3];
+            weight = new int[3];
+        }
+        public void init_Profit(int p1,int p2,int p3)
+        {
+            profit[0] = p1;
+            profit[1] = p2;
+            profit[2] = p3;
+        }
+
+        public void init_Weight(int w1, int w2, int w3)
+        {
+            weight[0] = w1;
+            weight[1] = w2;
+            weight[2] = w3;
+        }
+
+        public int get_Profit(int index)
+        {
+            return profit[index];
+        }
+
+        public int get_Weight(int index)
+        {
+            return weight[index];
+        }
+    }
+}
