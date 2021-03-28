5c314d0c068767a81c10187616c2903e881d1dca
AoYangJia <1357878465@qq.com>
2021-03-28 17:39:32 +0800

    注释全部完成

--- item_Set.cs
+++ item_Set.cs
@@ -6,9 +6,13 @@ namespace D_01_Bag
 {
     public class item_Set
     {
+        //价值数组
         private int[] profit;
+        //重量数组
         private int[] weight;
+        //第三组数据的价值重量比
         private double radio;
+        //三个物品的选择情况
         private int[] selected;
         public item_Set()
         {
@@ -18,15 +22,19 @@ namespace D_01_Bag
             radio = 0.0;
         }
 
+        //初始化价值重量比
         public void init_Radio()
         {
             radio = (profit[2] * 1.0) / (weight[2]);
         }
 
+        //获取第三个物品的价值重量比
         public double get_Radio()
         {
             return radio;
         }
+
+        //初始化价值数组
         public void init_Profit(int p1,int p2,int p3)
         {
             profit[0] = p1;
@@ -34,6 +42,7 @@ namespace D_01_Bag
             profit[2] = p3;
         }
 
+        //初始化重量数组
         public void init_Weight(int w1, int w2, int w3)
         {
             weight[0] = w1;
@@ -41,21 +50,26 @@ namespace D_01_Bag
             weight[2] = w3;
         }
 
+        //获取第i个物品价值
         public int get_Profit(int index)
         {
             return profit[index];
         }
 
+        //获取第i个物品重量
         public int get_Weight(int index)
         {
             return weight[index];
         }
 
+        //设置第i个物品被选中
         public void set_Selected(int i)
         {
             selected = new int[3];
             selected[i] = 1;
         }
+
+        //获取选中情况
         public int get_Selected()
         {
             for (int i = 0; i < 3; i++)
