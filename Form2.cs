15602f6f0211e32b8ecf8b5e7a42a5bfa0368c0e
AoYangJia <1357878465@qq.com>
2021-03-28 17:26:39 +0800

    完善了绘制散点图的类的注释

--- Form2.cs
+++ Form2.cs
@@ -11,7 +11,9 @@ namespace D_01_Bag
     public partial class Form2 : Form
     {
         Form1 form1;
+        //index0是选中的数据集的下标
         int index0;
+        //将form1传入
         public Form2(Form1 form, int index)
         {
             InitializeComponent();
@@ -19,14 +21,20 @@ namespace D_01_Bag
             index0 = index;
         }
 
+        //直接在显示时绘图
         private void Form2_Shown(object sender, EventArgs e)
         {
+            //获取当前图形
             Graphics g = this.CreateGraphics();
+
+            //创建新原点
             PointF origin = new PointF(40, this.Height - 80);
+            //绘制坐标系的初始线
             g.DrawLine(new Pen(Brushes.Black, 2), origin, new PointF(this.Width, this.Height - 80));
             g.DrawLine(new Pen(Brushes.Black, 2), origin, new PointF(40, 0));
             PointF p1;
             PointF p2;
+            //绘制刻度
             for (int i = 1; i * 20 + 40 < this.Width; i++)
             {
                 p1 = new PointF(40 + i * 20, this.Height - 80);
@@ -38,6 +46,7 @@ namespace D_01_Bag
                     g.DrawString((i * 20).ToString(), new Font("黑体", 8, FontStyle.Bold), new SolidBrush(Color.Black), p2);
                 }
             }
+            //绘制刻度下方的字符串
             for (int i = 1; i * 20 + 40 < this.Height; i++)
             {
                 g.DrawLine(new Pen(Brushes.Black, 1), new PointF(40, this.Height - 80 - 20 * i), new PointF(45, this.Height - 80 - i * 20));
@@ -47,23 +56,29 @@ namespace D_01_Bag
                     g.DrawString((i * 20).ToString(), new Font("黑体", 8, FontStyle.Bold), new SolidBrush(Color.Black), p2);
                 }
             }
+            //绘制字符串x和y和原点0
             p2 = new PointF(30, this.Height - 75);
             g.DrawString("0", new Font("黑体", 8, FontStyle.Bold), new SolidBrush(Color.Black), p2);
             p2 = new PointF(this.Width - 30, this.Height - 77);
             g.DrawString("x", new Font("黑体", 8, FontStyle.Bold), new SolidBrush(Color.Black), p2);
             p2 = new PointF(30, 5);
             g.DrawString("y", new Font("黑体", 8, FontStyle.Bold), new SolidBrush(Color.Black), p2);
+
+            //获取数据集
             data_Set_Block data_Block = form1.get_Data_Block(index0);
+            //获取数据组个数
             int item_Count = data_Block.get_Item_Count();
             for (int i = 0; i < item_Count; i++)
             {
                 item_Set item = data_Block.get_Item(i);
+                //绘制当前数据组
                 for(int j = 0; j < 3; j++)
                 {
                     draw_Point(item.get_Profit(j), item.get_Weight(j));
                 }
             }
         }
+        //在以orign为原点的向上为y轴向右为x轴的坐标系中画点（x，y）
         private void draw_Point(int x, int y)
         {
             Graphics g = this.CreateGraphics();
